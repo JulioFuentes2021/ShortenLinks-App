35184fd7d3e0b459cc864b34e2ea91da3ae4fd4a
Julio Fuentes <juliodddddd1234@gmail.com>
2021-10-31 11:24:40 -0600

    React router is implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/pages/App';
+import App from '../src/pages/routers/index';
 // import App from './components/Cards';
 import { Provider } from '../src/Context/index';
 
